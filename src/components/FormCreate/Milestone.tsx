6b71d7a25fa0f017121994db56c4b4e92b21c095
Mane <angelillo3581@gmail.com>
2024-05-16 12:34:32 -0600

    formateo

--- src/components/FormCreate/Milestone.tsx
+++ src/components/FormCreate/Milestone.tsx
@@ -18,7 +18,8 @@ function Milestone({ id, value, handleInputChange, handleRemoveObjective }: Mile
                         X
                     </button>
                 )}
-            </div>                    <label className="block text-md text-white font-light pt-1 mb-5">Puedes escribir de 200 a 500 palabras</label>
+            </div>
+            <label className="block text-md text-white font-light pt-1 mb-5">Puedes escribir de 200 a 500 palabras</label>
             <div className="w-full mb-4 border border-gray-800 rounded-lg bg-gray-700 dark:bg-gray-700 dark:border-gray-600">
                 <div className="px-4 py-2 bg-gray-600 rounded-b-lg dark:bg-gray-800">
                     <textarea id={`editor${id}`} value={value} onChange={(e) => handleInputChange(id, e.target.value)} rows={8} className="block w-full px-0 text-sm text-gray-800 bg-gray-600 border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Escribe un artículo..."></textarea>
